7300fa3ccb6549163fd9dbac1e9390a353f99c49
Nerfay <101548076+Nerfay@users.noreply.github.com>
2023-11-07 14:26:09 +0200

    Add files via upload

---
+++ 8 laba/Laba_7_V_1/Product.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace Laba_7_V_1
+{
+    [Serializable]
+    public class Product
+    {
+        private double _price;
+        private string _name;
+        private string _brand;
+        private bool _isPromotional;
+        private double _promotionalPrice;
+
+        public Product(double price, string name, string brand, double promotionalPrice=double.NaN)
+        {
+            Price = price;
+            Name = name;
+            Brand = brand;
+            if (!double.IsNaN(promotionalPrice))
+            {
+                IsPromotional = true;
+                PromotionalPrice = promotionalPrice;
+            }
+            else
+            {
+                IsPromotional = false;
+            }
+        }
+
+        [JsonPropertyName("Price")]
+        /// <summary>
+        ///СВойство цена продукта , возвращает ArgumentOutOfRangeException, если пытаемся присвоить отрицательное значение 
+        /// </summary>
+        public double Price
+        {
+            get => _price;
+            init
+            {
+                if (value > 0 )
+                {
+                    _price = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+
+        [JsonPropertyName("Name")]
+        /// <summary>
+        /// Свойство название продукта , возвращает ArgumentNullException, если пытаемся присвоить пустую строку
+        /// </summary>
+        public string Name
+        {
+            get => _name;
+            init
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    _name = value.Trim();
+                }
+                else
+                {
+                    throw new ArgumentNullException();
+                }
+            }
+        }
+
+        [JsonPropertyName("Brand")]
+        /// <summary>
+        /// Свойство бренд,фирма продукта , возвращает ArgumentNullException, если пытаемся присвоить пустую строку
+        /// </summary>
+        public string Brand
+        {
+            get => _brand;
+            init
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    _brand = value.Trim();
+                }
+                else
+                {
+                    throw new ArgumentNullException();
+                }
+            }
+        }
+
+        [JsonPropertyName("IsPromotional")]
+        /// <summary>
+        /// Свойство акционный ли товар 
+        /// </summary>
+        public bool IsPromotional
+        {
+            get => _isPromotional;
+            init => _isPromotional = value;
+        }
+
+        [JsonPropertyName("PromotionalPrice")]
+        /// <summary>
+        ///СВойство акционная цена продукта , возвращает ArgumentOutOfRangeException, если пытаемся присвоить отрицательное значение или значение больше обычной цены
+        /// </summary>
+        public double PromotionalPrice
+        {
+            get => _promotionalPrice;
+            init
+            {
+                if (value >= 0 && value<Price)
+                {
+                    _promotionalPrice = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+        public override string ToString()
+        {
+            string IspromotionalString = "";
+            if (IsPromotional)
+            {
+                IspromotionalString = $"promotional price= {PromotionalPrice} ;";
+            }
+            return $"Product(name = {Name} ; brand= {Brand} ;price= {Price} ; {IspromotionalString})\n";
+        }
+    }
+}
---
+++ 8 laba/Laba_7_V_1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Laba_7_V_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product(price: 10.33, name: "bread", brand: "OMXA",promotionalPrice:9.3);
+            Product product2 = new Product(price: 10.33, name: "dress", brand: "VS", promotionalPrice: 3.3);
+            Product product3 = new Product(price: 100000000000000, name: "car", brand: "OPEL");
+            Product product4 = new Product(price: 700.450, name: "iphone", brand: "Apple", promotionalPrice: 600);
+            Product product5 = new Product(price: 9.99, name: "cup", brand: "My Home", promotionalPrice: 9.3);
+            Product product6 = new Product(price: 6501.33, name: "t-short", brand: "lives");
+
+            ShoppingList shoppingList = new ShoppingList(product3);
+            //Добавление елементов product6,product2,product1,product5
+            shoppingList.AddElement(product6);
+            shoppingList.AddElement(product2);
+            shoppingList.AddElement(product1);
+            shoppingList.AddElement(product5);
+            shoppingList.AddElement(product4);
+
+            // Сериализация 
+            string fileNameJson = "ShopingList.json";
+            string fileNameBinary = "ShopingList.bin";
+
+            FileManager.SerializationToJSON(shoppingList.ProductList, fileNameJson);
+            FileManager.SerializationToBinary(shoppingList.ProductList, fileNameBinary);
+
+            // Десериализация 
+            
+            List<Product> listProductJson = (List<Product>)FileManager.DeserializationFromJSON(fileNameJson, new List<Product>());
+            Console.WriteLine($"From {fileNameJson}  :");
+            foreach (Product product in listProductJson)
+            {
+                Console.WriteLine(product);
+                Console.WriteLine("------------------------");
+            }
+
+            List<Product> listProductBinary = (List<Product>)FileManager.DeserializationFromBinary(fileNameBinary);
+            Console.WriteLine($"From {fileNameBinary}  :");
+            foreach (Product product in listProductBinary)
+            {
+                Console.WriteLine(product);
+                Console.WriteLine("------------------------");
+            }
+            
+        }
+    }
+}
